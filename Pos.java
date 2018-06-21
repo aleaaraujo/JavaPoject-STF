ae0f11a8be538957c0acb6bb04c547492d738770
alisson10xp <39743423+alisson10xp@users.noreply.github.com>
2018-06-21 15:19:09 -0300

    Update Pos.java

--- Pos.java
+++ Pos.java
@@ -2,6 +2,10 @@ public class Pos extends LinhaTelefonica{
 	 
 	private Fatura[] faturas; 
 	
+	public Pos(String num, Cliente cli, Fatura[] fatura) {
+		super(num, cli);
+		this.faturas = fatura;
+	}
 	public boolean pagarFatura (){
 	
 		return true;
@@ -18,4 +22,21 @@ public class Pos extends LinhaTelefonica{
 		aux+="\nFATURA: "+this.faturas;		
 		return aux;
 	}
+	public boolean pagarFatura(){      //teste
+		Fatura [] fat = getFatura();
+		Calendar agora = Calendar.getInstance();
+		long h3 = agora.getTimeInMillis();
+		double valor = 0;
+		for(int x=0; x<fat.length; x++){
+			long h1 = fat[x].getDataFinal().getTimeInMillis();
+			if(h1 < h3){
+				valor += fat[x].getValor();
+			}
+		}
+		if(valor > 0){
+			return true;
+		}else{
+			return false;
+		}
+	} 
 }
