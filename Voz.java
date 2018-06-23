770594954d20fdf866b18c875ec585b2ef9b4797
alisson10xp <39743423+alisson10xp@users.noreply.github.com>
2018-06-23 17:12:08 -0300

    Update Voz.java

--- Voz.java
+++ Voz.java
@@ -2,8 +2,12 @@ public class Voz extends Chamada {
 
 	private Calendar termino;
 	
-	public void setTermino(Calendar ter){
-		this.termino = ter;
+	public Voz (int transmissaoId, Calendar horario, LinhaTelefonica destinatario, float custo, Calendar termino){
+		super(transmissaoId, horario,destinatario, custo);
+		this.termino = termino;
+	}
+	public void setTermino(Calendar termino){
+		this.termino = termino;
 	}
 	public Calendar getTermino (){
 		return this.termino;
@@ -11,7 +15,7 @@ public class Voz extends Chamada {
 	public String toString (){
 		String aux = "";	
 		aux +=super.toString();
-		aux +="TERMINO: "this.termino;		
+		aux +="\nTERMINO: "+this.termino;		
 		retunr aux;
 	}
 }
